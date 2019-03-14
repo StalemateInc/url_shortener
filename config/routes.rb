a969d086e8e0504d73dcc4e5d0ed8b0d0b19f28e
Egor Dovnar <egordovnar.job@gmail.com>
2019-03-14 11:49:26 +0300

    Configured database and added some boilerplate code.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'main#index'
+
+  namespace :api do
+    namespace :v1 do
+
+    end
+  end
+
 end
