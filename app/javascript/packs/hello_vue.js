b8a0596abd0066f4025f31a003dbe17dd0d1ed4d
Egor Dovnar <egordovnar.job@gmail.com>
2019-03-14 12:21:41 +0300

    Added bootstrap-vue.

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -7,6 +7,11 @@
 
 import Vue from 'vue'
 import App from '../app.vue'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.min.css'
+
+Vue.use(BootstrapVue);
 
 document.addEventListener('DOMContentLoaded', () => {
   const el = document.body.appendChild(document.createElement('hello'));
